143fde27e3bf44e19e54b7b1526b14edefd89424
Daria Sydorenko <dashasidorenko123@gmail.com>
2024-07-04 15:19:08 +0300

    make build

---
+++ index.88eebfc6.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(e){e.preventDefault();var t=document.getElementById("largeImg"),n=e.target.closest(".list-item__link");n&&(t.src=n.href)});
+//# sourceMappingURL=index.88eebfc6.js.map
